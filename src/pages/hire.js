7757b2baefa20e9afd81225c1e9bee26249ab05e
Michael Meyer <mtmeyer32@gmail.com>
2020-02-05 21:32:30 -0500

    Add coming soon page to all.

---
+++ src/pages/hire.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ComingSoon from "react-coming-soon"
+
+export default () => (
+
+    <ComingSoon
+        image="https://react-coming-soon.maksv.me/default-image.jpeg"
+        bgColor="#fff"
+        textColor="#000"
+        illustration="development"
+      />
+) 
--- src/pages/travels.js
+++ src/pages/travels.js
@@ -1,10 +1,12 @@
 import React from "react"
-import Layout from "../components/layout"
+import ComingSoon from "react-coming-soon"
+
 export default () => (
-<Layout>
-    <h1>Travels</h1>
-    <div>
-      Some stuff.
-    </div>
-</Layout>
-)
+
+    <ComingSoon
+        image="https://react-coming-soon.maksv.me/default-image.jpeg"
+        bgColor="#fff"
+        textColor="#000"
+        illustration="development"
+      />
+) 
